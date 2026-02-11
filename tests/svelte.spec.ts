6a902bc67992dcf3085bc9faf4dde25d35dcdd76
Chun Nam Wong <chunnamwong@users.noreply.github.com>
2026-02-12 08:15:32 +0900

    test(svelte): ensure SSR works with HMR

--- tests/svelte.spec.ts
+++ tests/svelte.spec.ts
@@ -8,18 +8,31 @@ test.describe('dev server', () => {
 		// Only compare the icons so that the test can run aross platform
 		await expect(page.getByRole('article')).toHaveScreenshot();
 	});
+});
 
-	test('handles hot reload correctly', async ({ page, browserName }) => {
-		const iconFolderPath = path.join(process.cwd(), `examples/svelte/src/lib/assets/icons`);
-		const testRoute = `/test-hot-reload-${browserName}`;
-		const testPagePath = path.join(
-			process.cwd(),
-			`examples/svelte/src/routes/${testRoute}/+page.svelte`,
-		);
+test.describe('dev server with new files', () => {
+	const iconFolderPath = path.join(process.cwd(), `examples/svelte/src/lib/assets/icons`);
+	let testRoute: string;
+	let testPagePath: string;
+
+	test.beforeAll(async ({ browserName }) => {
+		testRoute = `/test-hot-reload-${browserName}`;
+		testPagePath = path.join(process.cwd(), `examples/svelte/src/routes/${testRoute}/+page.svelte`);
+	});
+
+	test.beforeEach(async ({ page }) => {
 		await fs.mkdir(path.dirname(testPagePath), { recursive: true });
 		await fs.writeFile(testPagePath, '');
-		// Wait for the dev server
-		await new Promise((resolve) => setTimeout(resolve, 500));
+		await page.waitForTimeout(500);
+	});
+
+	test.afterEach(async () => {
+		await fs.rm(path.join(iconFolderPath, 'test-home-copy.svg'));
+		await fs.rm(testPagePath);
+		await fs.rmdir(path.dirname(testPagePath));
+	});
+
+	test('handles hot reload correctly', async ({ page }) => {
 		await page.goto(testRoute);
 		await fs.copyFile(
 			path.join(iconFolderPath, 'home.svg'),
@@ -42,6 +55,10 @@ test.describe('dev server', () => {
 </style>
 `,
 		);
+		await expect(page).toHaveScreenshot();
+
+		// Refresh to ensure SSR works with hot reload
+		await page.reload();
 		await expect(page).toHaveScreenshot();
 	});
 });
